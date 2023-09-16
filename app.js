2114df7a5b444a9fbdc6b8e9d272aec6a97ae110
Chandu7702984063 <137478403+Chandu7702984063@users.noreply.github.com>
2023-09-16 12:11:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// Define API key and get references to HTML elements
 const API_KEY = 'da8df181';
 const searchInput = document.getElementById('searchInput');
 const suggestions = document.getElementById('suggestions');
 const favoriteMoviesList = document.getElementById('favoriteMovies');
 const favoriteMovies = [];
 
+// Add an event listener to the search input with a debounce function
 searchInput.addEventListener('input', debounce(handleSearch, 300));
 
+// Define a debounce function to delay search input processing
 function debounce(func, delay) {
     let timer;
     return function () {
@@ -14,6 +17,7 @@ function debounce(func, delay) {
     };
 }
 
+// Handle the movie search based on user input
 function handleSearch() {
     const searchTerm = searchInput.value.trim();
     if (searchTerm.length === 0) {
@@ -21,12 +25,14 @@ function handleSearch() {
         return;
     }
 
+    // Fetch movie data from the API and display search results
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}`)
         .then(response => response.json())
         .then(data => {
             const results = data.results;
             suggestions.innerHTML = '';
             results.forEach(movie => {
+                // Create and display a movie item with add to favorites and details buttons
                 const movieItem = document.createElement('div');
                 movieItem.classList.add('suggestion');
                 movieItem.innerHTML = `
@@ -42,6 +48,7 @@ function handleSearch() {
         });
 }
 
+// Add a movie to the favorites list
 function addToFavorites(movieId, movieTitle) {
     if (!favoriteMovies.find(item => item.id === movieId)) {
         favoriteMovies.push({ id: movieId, title: movieTitle });
@@ -58,6 +65,7 @@ function addToFavorites(movieId, movieTitle) {
     }
 }
 
+// Update the favorites list in the HTML
 function updateFavoritesList() {
     favoriteMoviesList.innerHTML = '';
     favoriteMovies.forEach(movie => {
@@ -67,6 +75,7 @@ function updateFavoritesList() {
     });
 }
 
+// Remove a movie from the favorites list
 function removeFromFavorites(movieId) {
     const index = favoriteMovies.findIndex(item => item.id === movieId);
     if (index !== -1) {
@@ -75,6 +84,7 @@ function removeFromFavorites(movieId) {
     }
 }
 
+// Show movie details when the user clicks on the "Details" button
 function showMovieDetails(movieId) {
     fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`)
         .then(response => response.json())
